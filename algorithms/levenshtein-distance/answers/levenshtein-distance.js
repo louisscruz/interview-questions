50b60039d0d4590356f452e7bca3c1725769fd4d
Louis Cruz <louisstephancruz@me.com>
2016-12-20 17:52:25 -0800

    Initial commit and Levenshtein

---
+++ algorithms/levenshtein-distance/answers/levenshtein-distance.js
@@ -0,0 +1,20 @@
+String.prototype.levenshteinDistance = function(other) {
+  if (this.length === 0) { return other.length; }
+  if (other.length === 0) { return this.length; }
+
+  let cost;
+
+  if (this[this.length - 1] === other[other.length - 1]) {
+    cost = 0;
+  } else {
+    cost = 1;
+  }
+
+  const subLeft = this.slice(0, -1).levenshteinDistance(other);
+  const subRight = this.levenshteinDistance(other.slice(0, -1));
+  const subBoth = this.slice(0, -1).levenshteinDistance(other.slice(0, -1));
+
+  return Math.min(subLeft + 1, subRight + 1, subBoth + cost);
+}
+
+console.log('kitten'.levenshteinDistance('sitting') === 3);
---
+++ algorithms/levenshtein-distance/levenshtein-distance-prompt.md
@@ -0,0 +1,5 @@
+# Levenshtein Distance
+
+Levenshtein distance is defined as the number of edits – inserts, deletes, substitutions – that are required to turn one string into another.
+
+Create a function that finds the Levenshtein distance of one string to another.
