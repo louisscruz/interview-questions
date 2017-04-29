6a8f1817481fc9e8c8f27bd808ab2b244dac8c83
louisscruz <louisstephancruz@me.com>
2017-04-29 14:52:34 -0700

    Add spiral-matrix

---
+++ algorithms/spiral-matrix/answers/spiralMatrix.js
@@ -0,0 +1,49 @@
+const spiralMatrix = (arr, cb) => {
+  const directions = {
+    right: {
+      name: 'right',
+      next: 'down',
+      remaining: arr[0].length,
+      delta: [0, 1],
+    },
+    down: {
+      name: 'down',
+      next: 'left',
+      remaining: arr.length - 1,
+      delta: [1, 0],
+    },
+    left: {
+      name: 'left',
+      next: 'up',
+      remaining: arr[0].length - 1,
+      delta: [0, -1],
+    },
+    up: {
+      name: 'up',
+      next: 'right',
+      remaining: arr.length - 2,
+      delta: [-1, 0],
+    },
+  };
+
+  let currentDirection = directions.right;
+  let currentPosition = [0, -1];
+
+  const currentElement = () => arr[currentPosition[0]][currentPosition[1]];
+  const updatePosition = () => {
+    currentPosition = currentPosition.map((el, i) => {
+      return el + currentDirection.delta[i]
+    });
+  };
+
+  while (currentDirection.remaining) {
+    for (let i = 0; i < currentDirection.remaining; i += 1) {
+      updatePosition();
+      cb(currentElement());
+    }
+    currentDirection.remaining -= 2;
+    currentDirection = directions[currentDirection.next];
+  }
+
+  return arr;
+}
---
+++ algorithms/spiral-matrix/spiral-matrix.md
@@ -0,0 +1,17 @@
+# Spiral Matrix
+
+Given a matrix (assume an array of arrays), iterate through the matrix
+in a spiral-like manner.
+
+For example, consider the following matrix:
+
+```
+[
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+```
+
+The order of the traversal of this matrix should be `1, 2, 3, 6, 9, 8,
+7, 4, 5`. 
